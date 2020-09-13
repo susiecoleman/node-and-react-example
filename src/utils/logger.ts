896a35ff64cbffb77893da06d0f3443ca193b338
Susie Coleman <git@susiecoleman.me>
2020-09-13 16:46:21 +0100

    Add log format

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,26 +1,30 @@
 import { createLogger, format, transports, Logger } from 'winston';
 import { getEnvVariables } from '../utils/config';
 
-const { combine, timestamp, colorize, simple, json } = format;
+const { combine, timestamp, colorize, simple, json, printf } = format;
 const { Console, File } = transports;
 
 const { LOG_LEVEL } = getEnvVariables('LOG_LEVEL');
+
+const logFormat = printf(logInfo => {
+  return `${logInfo.timestamp} [${logInfo.label}] ${logInfo.level}: ${logInfo.message}`;
+});
 
 const logger: Logger = createLogger({
   transports: [
     new File({
       filename: 'application.log',
       level: LOG_LEVEL,
-      format: combine(timestamp(), json()),
+      format: combine(timestamp(), logFormat, json()),
     }),
     new File({
       filename: 'error.log',
       level: 'error',
-      format: combine(timestamp(), json()),
+      format: combine(timestamp(), logFormat, json()),
     }),
     new Console({
       level: 'info',
-      format: combine(colorize(), simple()),
+      format: combine(colorize(), logFormat, simple()),
     }),
   ],
 });
